7b820f6f30acac597a77bb4e75cffa3f70e2851c
Artur <magisterbrownie@gmail.com>
2024-05-20 18:52:37 +0200

    changed delay ids

--- delay.sh
+++ delay.sh
@@ -1,7 +1,9 @@
 delays() {
     ip netns exec clust$1 tc qdisc add dev veth$1a root netem delay $2ms
+    tc qdisc add dev veth$1b root netem delay $2ms
+
 }
-for ((i = 1; i <= 6; i++)); do
+for ((i = 1; i <= 7; i++)); do
     delays $i 50
 done
-delays 7 400 
+delays 8 400 
--- undelay.sh
+++ undelay.sh
@@ -1,5 +1,6 @@
 undelay() {
     ip netns exec clust$1 tc qdisc del dev veth$1a root 
+    tc qdisc del dev veth$1b root 
 }
 for ((i = 1; i <= 7; i++)); do
     undelay $i
