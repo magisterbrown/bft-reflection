a9eaa0c87a1a2cb3f96bba6d88c7518fa47077ce
magisterbrown <magisterbrownie@gmail.com>
2024-06-07 10:31:33 +0200

    different delays

--- delay.sh
+++ delay.sh
@@ -3,15 +3,15 @@ delays() {
     tc qdisc add dev veth$1b root netem delay $2ms
 
 }
-delays 1 80
-delays 2 80
-delays 3 75
-delays 4 80
-delays 5 75
-delays 6 80
-delays 7 80
-#for ((i = 2; i <= 7; i++)); do
-#    delays $i 80
+delays 1 45
+delays 2 45
+delays 3 40 
+delays 4 45
+delays 5 40
+delays 6 45
+delays 7 45
+#for ((i = 2; i <= 4; i++)); do
+#    delays $i 45
 #done
-delays 8 175
-delays 9 20
+delays 8 150
+delays 9 10
